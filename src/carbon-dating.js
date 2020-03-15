a73d1fe2c574c5f965f8bec9747c2b5db1f9ee59
Negorizont <boltuev01@mail.ru>
2020-03-15 10:26:55 +0400

    feat: complete carbon-dating and dream-team tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,10 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sample) {
+  if (typeof sample !== "string" || /[A-Za-z]/.test(sample) || sample <= 0 || sample > MODERN_ACTIVITY) return false;
+
+  let rate = Math.LN2.toFixed(3) / HALF_LIFE_PERIOD;
+  let years = Math.log(MODERN_ACTIVITY / parseFloat(sample)) / rate;
+  return Math.ceil(years);
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let teamName = [];
+  members.forEach(function(name) {
+    if (typeof(name) === 'string') {
+      for (let i=0; i<name.length;i++) {
+        if (name[i] !== ' ') {
+          teamName.push(name[i].toUpperCase());
+          break;
+        }
+      }
+    }
+  });
+  teamName = teamName.sort().join('');
+  return teamName;
 };
